d2ffc72e6ecf8cae4c1510ec036cb6bf5bb4fb69
nevasys <n.vahram@gmail.com>
2018-10-16 01:51:47 -0400

    Initial push
    first push

---
+++ AssuranceMGL7460/AssuranceMGL7460/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+// Useful linkf for us Alex:
+// http://fsharp.github.io/FSharp.Data/library/JsonProvider.html
+// https://msdn.microsoft.com/visualfsharpdocs/conceptual/visual-fsharp
+
+
+open FSharp.Data;
+open FSharp.Linq;
+
+
+
+// just an examples
+type Simple = JsonProvider<""" { "name":"John", "age":94 } """>
+let simple = Simple.Parse(""" { "name":"Tomas", "age":4 } """)
+
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
